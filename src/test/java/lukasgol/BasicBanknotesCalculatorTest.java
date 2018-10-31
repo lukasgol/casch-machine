a744c59ccda0cf1afc9edd0f0c689d4167c15990
Łukasz Goliński <lukasgol6900@gmail.com>
2018-10-31 15:03:59 +0100

    reformat code

--- src/test/java/lukasgol/BasicBanknotesCalculatorTest.java
+++ src/test/java/lukasgol/BasicBanknotesCalculatorTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertThat;
 
 public class BasicBanknotesCalculatorTest {
     private BasicBanknotesCalculator calculator;
+
     @Before
     public void setUp() {
         calculator = new BasicBanknotesCalculator();
@@ -17,42 +18,42 @@ public class BasicBanknotesCalculatorTest {
 
     @Test
     public void shouldReturnOneBanknoteOfEachValue() {
-        int[] state = new int[]{10,10,10,1};
+        int[] state = new int[]{10, 10, 10, 1};
         BanknotesAmount amount = calculator.calculateBanknotes(180, state);
-        assertThat(amount,is(new BanknotesAmount(1,1,1,1)));
+        assertThat(amount, is(new BanknotesAmount(1, 1, 1, 1)));
     }
 
     @Test
     public void shouldReturnOneBanknoteOf20And10And3Of50Value() {
-        int[] state = new int[]{0,10,10,1};
+        int[] state = new int[]{0, 10, 10, 1};
         BanknotesAmount amount = calculator.calculateBanknotes(180, state);
-        assertThat(amount,is(new BanknotesAmount(0,3,1,1)));
+        assertThat(amount, is(new BanknotesAmount(0, 3, 1, 1)));
 
     }
 
     @Test(expected = NotEnoughProperBanknotesException.class)
     public void shouldThrowNotEnoughProperBanknotesExceptionBecauseZero10BanknotesAmount() {
-        int[] state = new int[]{10,10,10,0};
+        int[] state = new int[]{10, 10, 10, 0};
         calculator.calculateBanknotes(180, state);
     }
 
     @Test(expected = NotEnoughProperBanknotesException.class)
     public void given70AmountShouldThrowNotEnoughProperBanknotesExceptionBecauseNotEnoughBanknotes() {
-        int[] state = new int[]{0,2,0,0};
+        int[] state = new int[]{0, 2, 0, 0};
         calculator.calculateBanknotes(70, state);
     }
 
     @Test()
     public void given70AmountShouldReturnOne50AndOne20() {
-        int[] state = new int[]{0,2,1,5};
+        int[] state = new int[]{0, 2, 1, 5};
         BanknotesAmount amount = calculator.calculateBanknotes(70, state);
-        assertThat(amount,is(new BanknotesAmount(0,1,1,0)));
+        assertThat(amount, is(new BanknotesAmount(0, 1, 1, 0)));
     }
 
     @Test()
     public void given70AmountShouldReturnOne50AndTwo10() {
-        int[] state = new int[]{0,2,0,2};
+        int[] state = new int[]{0, 2, 0, 2};
         BanknotesAmount amount = calculator.calculateBanknotes(70, state);
-        assertThat(amount,is(new BanknotesAmount(0,1,0,2)));
+        assertThat(amount, is(new BanknotesAmount(0, 1, 0, 2)));
     }
 }
